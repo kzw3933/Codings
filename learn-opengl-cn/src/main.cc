7523580258bcd9cf7ba1065fe4ae08033adf971e
kzw3933 <3099097649@qq.com>
2024-01-06 16:59:59 +0800

    三角形

--- learn-opengl-cn/src/main.cc
+++ learn-opengl-cn/src/main.cc
@@ -5,6 +5,23 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 int main()
 {
     /***
@@ -19,7 +36,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);  // 创建窗口, 这个窗口对象存放了所有和窗口相关的数据，而且会被GLFW的其他函数频繁地用到
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);  // 创建窗口, 这个窗口对象存放了所有和窗口相关的数据，而且会被GLFW的其他函数频繁地用到
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -28,6 +45,9 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+    // 设置回调函数,每当窗口改变大小，GLFW会调用这个函数并填充相应的参数供我们处理。
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     /**
      * 因为OpenGL只是一个标准/规范，具体的实现是由驱动开发商针对特定显卡实现的。由于OpenGL驱动版本众多，
      * 它大多数函数的位置都无法在编译时确定下来，需要在运行时查询。所以任务就落在了开发者身上，开发者需要
@@ -41,12 +61,148 @@ int main()
     }    
 
     /**
-     * 开始渲染之前还有一件重要的事情要做，必须告诉OpenGL渲染窗口的尺寸大小，即视口(Viewport)，
-     * 这样OpenGL才只能知道怎样根据窗口大小显示数据和坐标。
+     * 在OpenGL中，任何事物都在3D空间中，而屏幕和窗口却是2D像素数组，这导致OpenGL的大部分工作都是关于把3D坐标转变为适应
+     * 屏幕的2D像素。3D坐标转为2D坐标的处理过程是由OpenGL的图形渲染管线（Graphics Pipeline，大多译为管线，实际上指的是
+     * 一堆原始图形数据途经一个输送管道，期间经过各种变化处理最终出现在屏幕的过程）管理的。图形渲染管线可以被划分为两个主要
+     * 部分：第一部分把3D坐标转换为2D坐标，第二部分是把2D坐标转变为实际的有颜色的像素。图形渲染管线可以被划分为几个阶段，每
+     * 个阶段将会把前一个阶段的输出作为输入。所有这些阶段都是高度专门化的（它们都有一个特定的函数），并且很容易并行执行。正是
+     * 由于它们具有并行执行的特性，当今大多数显卡都有成千上万的小处理核心，它们在GPU上为每一个（渲染管线）阶段运行各自的小程序，
+     * 从而在图形渲染管线中快速处理你的数据。这些小程序叫做着色器(Shader)。有些着色器可以由开发者配置，因为允许用自己写的着色
+     * 器来代替默认的，所以能够更细致地控制图形渲染管线中的特定部分了。
+     * 图形渲染管线的第一个部分是顶点着色器(Vertex Shader)，它把一个单独的顶点作为输入。顶点着色器主要的目的是把3D坐标转为
+     * 另一种3D坐标，同时顶点着色器允许我们对顶点属性进行一些基本处理。图元装配(Primitive Assembly)阶段将顶点着色器输出的
+     * 所有顶点作为输入（如果是GL_POINTS，那么就是一个顶点），并所有的点装配成指定图元的形状；本节例子中是一个三角形。图元装
+     * 配阶段的输出会传递给几何着色器(Geometry Shader)。几何着色器把图元形式的一系列顶点的集合作为输入，它可以通过产生新顶点
+     * 构造出新的（或是其它的）图元来生成其他形状。例子中，它生成了另一个三角形。几何着色器的输出会被传入光栅化阶段(Rasterization
+     *  Stage)，这里它会把图元映射为最终屏幕上相应的像素，生成供片段着色器(Fragment Shader)使用的片段(Fragment)。在片段着色器运行
+     * 之前会执行裁切(Clipping)。裁切会丢弃超出你的视图以外的所有像素，用来提升执行效率。片段着色器的主要目的是计算一个像素的最终颜色
+     * 这也是所有OpenGL高级效果产生的地方。通常，片段着色器包含3D场景的数据（比如光照、阴影、光的颜色等等），这些数据可以被用来计算
+     * 最终像素的颜色。在所有对应颜色值确定以后，最终的对象将会被传到最后一个阶段，我们叫做Alpha测试和混合(Blending)阶段。这个阶段检测
+     * 片段的对应的深度（和模板(Stencil)）值，用它们来判断这个像素是其它物体的前面还是后面，决定是否应该丢弃。这个阶段也会
+     * 检查alpha值（alpha值定义了一个物体的透明度）并对物体进行混合(Blend)。所以，即使在片段着色器中计算出来了一个像素输出的颜色，在渲染
+     * 多个三角形的时候最后的像素颜色也可能完全不同。可以看到，图形渲染管线非常复杂，它包含很多可配置的部分。然而，对于大多数场合，我们只需
+     * 要配置顶点和片段着色器就行了。几何着色器是可选的，通常使用它默认的着色器就行了。
     */
-    glViewport(0, 0, 800, 600);   
+    // 构建着色器并编译
+    // 顶点着色器
+    /**
+     * 每个OpenGL对象都使用一个ID引用
+    */
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // 检查是否存在编译错误
+    {
+        int success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+    }
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  // 设置回调函数,每当窗口改变大小，GLFW会调用这个函数并填充相应的参数供我们处理。
+    // 片段着色器
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // 检查是否存在编译错误
+    {
+        int success;
+        char infoLog[512];
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+    }
+
+    // 链接着色器
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    {
+        int success;
+        char infoLog[512];
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // 创建三角形顶点数据用于渲染
+    float vertices1[] = {
+        -0.5f, -0.25f, 0.0f, // left
+        0.5f, -0.25f, 0.0f, // right
+        0.0f, 0.25f, 0.0f // top
+    };
+
+    float vertices2[] = {
+        -0.5f, 0.75f, 0.0f, // left
+        0.5f, 0.75f, 0.0f, // right
+        0.0f, 0.25f, 0.0f // bottom
+    };
+
+    // 创建顶点数组对象和顶点缓冲区对象
+    unsigned int VBO1, VAO1, VBO2, VAO2;
+    glGenVertexArrays(1, &VAO1);
+    glGenBuffers(1, &VBO1);
+    glGenVertexArrays(2, &VAO2);
+    glGenBuffers(2, &VBO2);
+
+    /**
+     * 顶点数组对象(Vertex Array Object, VAO)可以像顶点缓冲对象那样被绑定，任何随后的顶点属性调用都会储存在这个VAO中。
+     * 这样的好处就是，当配置顶点属性指针时，你只需要将那些调用执行一次，之后再绘制物体的时候只需要绑定相应的VAO就行了。这使
+     * 在不同顶点数据和属性配置之间切换变得非常简单，只需要绑定不同的VAO就行了。刚刚设置的所有状态都将存储在VAO中。
+     * 一个顶点数组对象会储存以下这些内容：
+     *      - glEnableVertexAttribArray和glDisableVertexAttribArray的调用。
+     *      - 通过glVertexAttribPointer设置的顶点属性配置。
+     *      - 通过glVertexAttribPointer调用与顶点属性关联的顶点缓冲对象。
+     * 要想使用VAO，要做的只是使用glBindVertexArray绑定VAO。从绑定之后起，我们应该绑定和配置对应的VBO和属性指针，之后解绑
+     * VAO供之后使用。
+    */
+    // 先绑定顶点数组对象、顶点缓冲并配置顶点属性
+    glBindVertexArray(VAO1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    // 将定义的顶点数据复制到缓冲的内存中
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+
+    /**
+     * 顶点着色器允许我们指定任何以顶点属性为形式的输入。这使其具有很强的灵活性的同时，它还的确意味着我们
+     * 必须手动指定输入数据的哪一个部分对应顶点着色器的哪一个顶点属性。所以，我们必须在渲染前指定OpenGL该
+     * 如何解释顶点数据。第一个参数指定我们要配置的顶点属性。我们在顶点着色器中使用layout(location = 0)
+     * 定义了position顶点属性的位置值(Location)。它可以把顶点属性的位置值设置为0。每个顶点属性从一个VBO
+     * 管理的内存中获得它的数据，而具体是从哪个VBO（程序中可以有多个VBO）获取则是通过在调用glVertexAttribPointer
+     * 时绑定到GL_ARRAY_BUFFER的VBO决定的。由于在调用glVertexAttribPointer之前绑定的是先前定义的VBO对象，
+     * 顶点属性0现在会链接到它的顶点数据。
+    */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // 使用glVertexAttribPointer时已经把顶点属性链接到VBO的顶点数据了，因此这里可以安全unbind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // unbind VAO, 防止其他的VAO calls偶然修改了VAO(尽管这几乎不会发生)
+    glBindVertexArray(0);
+
+    // 对于第二个三角形重复以上操作
+    glBindVertexArray(VAO2);       
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);      
+
 
     // 渲染循环
     while(!glfwWindowShouldClose(window))
@@ -61,10 +217,27 @@ int main()
         */
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+
+        // 绘制我们的三角形1
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // 绘制我们的三角形2
+        glBindVertexArray(VAO2);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         // 检查并调用事件，交换缓冲
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
+
+    glDeleteVertexArrays(1, &VAO1);
+    glDeleteBuffers(1, &VBO1);
+    glDeleteVertexArrays(1, &VAO2);
+    glDeleteBuffers(1, &VBO2);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
 
